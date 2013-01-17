b7e2efc0c8d1901ef6853f964e40030bbcd3f798
Jon Pugh <jon@thinkdrop.net>
2013-01-17 23:42:31 +0000

    adding the package installer method

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,20 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+
+# this is the simple way to get started.
+# We let apt handle installing aegir... for now
+
+apt_repository "koumbit-stable" do
+  uri "http://debian.koumbit.net/debian"
+  components ["stable", "main"]
+  key "http://debian.koumbit.net/debian/key.asc"
+  action :add
+end
+
+require_recipe "apt"
+require_recipe "sudo"
+package "aegir"
+
+
