c776bffba119049da6f110ccf00ee8befecdec2f
Jonathan Pugh <jon@thinkdrop.net>
2013-01-18 13:11:40 -0500

    attempting to add ssh keys

--- recipes/users.rb
+++ recipes/users.rb
@@ -19,3 +19,18 @@ directory "/home/jon" do
   action :create
   recursive true
 end 
+directory "/home/jon/.ssh" do
+  owner "jon"
+  group "jon"
+  mode "0755"
+  action :create
+  recursive true
+end 
+cookbook_file "/home/jon/.ssh/authorized_keys" do
+  source "ssh/id_rsa.pub.jon"
+  action :create
+  backup false
+  owner "jon"
+  group "jon"
+  mode "0600"
+end
