309f54c0b4f406b74cf189bbd9da9e9368ec486c
Jon Pugh <jon@thinkdrop.net>
2013-01-18 18:50:05 -0500

    adding an aegir.rb installation script

---
+++ recipes/aegir.rb
@@ -0,0 +1,8 @@
+
+
+git "DevShop Provision Source" do
+    repository "http://git.drupal.org/project/devshop_provision.git"
+    reference "6.x-1.x"
+    action :checkout
+    destination "/home/jon/source/devshop_provision"
+end
