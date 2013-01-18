7cdb8fb0b4959e9e40ea3ab1bf69f70d50918542
Jon Pugh <jon@thinkdrop.net>
2013-01-18 18:19:16 -0500

    removing copied examples

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,26 +1,5 @@
 #
-# Cookbook Name:: devudo-cookbooks
+# Cookbook Name:: devudo
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
 
-
-# this is the simple way to get started.
-# We let apt handle installing aegir... for now
-
-apt_repository "koumbit-stable" do
-  uri "http://debian.koumbit.net/debian"
-  components ["stable", "main"]
-  key "http://debian.koumbit.net/debian/key.asc"
-  action :add
-end
-
-require_recipe "apt"
-require_recipe "sudo"
-require_recipe "mysql"
-require_recipe "mysql_secure_installation"
-
-package "aegir"
