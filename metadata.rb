9d9258699ace0a7e27b5c0b96513f67167775647
Jonathan Pugh <jon@thinkdrop.net>
2013-01-18 13:26:13 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Jon Pugh"
+maintainer_email "jon@thinkdrop.net"
 license          "All rights reserved"
 description      "Installs/Configures devudo-cookbooks"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
