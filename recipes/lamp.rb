0b87ceab52720ddd5608348a58f75a272495f6e3
Jon Pugh <jon@thinkdrop.net>
2013-01-18 18:33:28 -0500

    adding basic lamp recipe.

---
+++ recipes/lamp.rb
@@ -0,0 +1,16 @@
+#
+#
+#
+
+# Install required apt packages.
+%w{ apache2 php5 php5-cli php5-gd php5-mysql postfix sudo rsync git-core unzip }.each do |package_name|
+  package package_name
+end
+
+service "apache2" do
+  service_name "apache2"
+  restart_command "/usr/sbin/invoke-rc.d apache2 restart && sleep 1"
+  reload_command "/usr/sbin/invoke-rc.d apache2 reload && sleep 1"
+  supports [:restart, :reload, :status]
+  action :enable
+end
