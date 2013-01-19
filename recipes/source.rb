a0d70591f8146e1bbdf7b75b4a5dacb2730de696
Jon Pugh <jon@thinkdrop.net>
2013-01-18 19:27:05 -0500

    adding the first of many source code checkouts

---
+++ recipes/source.rb
@@ -0,0 +1,15 @@
+
+directory "/src" do
+  owner "root"
+  group "devs"
+  mode "0775"
+  action :create
+  recursive true
+end
+
+git "DevShop Provision Source" do
+    repository "http://git.drupal.org/project/devshop_provision.git"
+    reference "6.x-1.x"
+    action :sync
+    destination "/src/devshop_provision"
+end
