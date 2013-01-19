76ce53b531b0e521ab856a0f57bd776154666e0f
Jon Pugh <jon@thinkdrop.net>
2013-01-19 12:06:25 -0500

    cleaning up

--- recipes/aegir.rb
+++ recipes/aegir.rb
@@ -1,72 +1,48 @@
 
 # From http://community.aegirproject.org/installing/manual
 
-#
+# Aegir needs lamp
 require_recipe "devudo::lamp"
 
+# Create the Aegir user
+user "aegir" do
+  comment "Aegir Hosting Management"
+  home "/var/aegir"
+  shell "/bin/bash"
+  system true
+end
+group "www-data" do
+  action :modify
+  members "aegir"
+  append true
+end
+directory "/var/aegir" do
+  owner "aegir"
+  group "aegir"
+  mode 00755
+  action :create
+  recursive true
+end
 
-  # Create the Aegir user
-  user "aegir" do
-    comment "Aegir Hosting Management"
-    home "/var/aegir"
-    shell "/bin/bash"
-    system true
-  end
-  group "www-data" do
-    action :modify
-    members "aegir"
-    append true
-  end
-  directory "/var/aegir" do
-    owner "aegir"
-    group "aegir"
-    mode 00755
-    action :create
-    recursive true
-  end
+# Make sure the Aegir user is allowed to restart apache
+package 'sudo' do
+  action :install
+end
+cookbook_file "/etc/sudoers.d/aegir" do
+  source "sudoers.d/aegir"
+  action :create
+  backup false
+  owner "root"
+  group "root"
+  mode 00440
+end
 
-  # Make sure the Aegir user is allowed to restart apache
-  # RESOURCE sudo DOES NOT EXIST
-  
-  #sudo "apache2" do
-  #  user "aegir"
-  #  commands ["/usr/sbin/apache2ctl"]
-  #  host "ALL"
-  #  nopasswd true
-  #end
+# symlink apache config
+link "/etc/apache2/conf.d/aegir.conf" do
+  to "/var/aegir/apache.conf"
+end
 
-  package 'sudo' do
-    action :install
-  end
-  cookbook_file "/etc/sudoers.d/aegir" do
-    source "sudoers.d/aegir"
-    action :create
-    backup false
-    owner "root"
-    group "root"
-    mode 00440
-  end
-
-#  file "/etc/sudoers.d/aegir" do
-#    owner "root"
-#    group "root"
-#    mode 00440
-#    action :create
-#    content "Defaults:aegir  !requiretty
-#aegir ALL=NOPASSWD: /usr/sbin/apache2ctl"
-#  end
-
-  # symlink apache config
-  link "/etc/apache2/conf.d/aegir.conf" do
-    to "/var/aegir/apache.conf"
-  end
-
-# a2enmod rewrite
-# ln -s /var/aegir/config/apache.conf /etc/apache2/conf.d/aegir.conf
-# Create a file at /etc/sudoers.d/aegir and add the following:
-#
-#Defaults:aegir  !requiretty
-#aegir ALL=NOPASSWD: /usr/sbin/apache2ctl
-#After saving, change the permissions on the file:
-#
-#chmod 0440 /etc/sudoers.d/aegir
+# Enable mod_rewrite
+exec "Enable mod_rewrite" do
+  command "a2enmod rewrite"
+end
