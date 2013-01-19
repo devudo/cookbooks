80d36e93b35846d649b97025d16f3d311dc4f86f
Jon Pugh <jon@thinkdrop.net>
2013-01-18 19:26:36 -0500

    adding drush

--- recipes/lamp.rb
+++ recipes/lamp.rb
@@ -3,7 +3,7 @@
 #
 
 # Install required apt packages.
-%w{ apache2 php5 php5-cli php5-gd php5-mysql postfix sudo rsync git-core unzip }.each do |package_name|
+%w{ apache2 php5 php5-cli php5-gd php5-mysql php-pear php5-curl mysql-server postfix sudo rsync git-core unzip }.each do |package_name|
   package package_name
 end
 
@@ -13,4 +13,10 @@ service "apache2" do
   reload_command "/usr/sbin/invoke-rc.d apache2 reload && sleep 1"
   supports [:restart, :reload, :status]
   action :enable
+end
+
+php_pear "drush" do
+  action :install_if_missing
+  channel "pear.drush.org"
+  version "4.6.0"
 end
