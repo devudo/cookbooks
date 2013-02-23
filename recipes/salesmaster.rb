d3784b4c18b564fdc8cd5cb882746f0469907fcc
Jon Pugh <jon@thinkdrop.net>
2013-02-23 13:22:13 -0500

    adding salesmaster recipe

---
+++ recipes/salesmaster.rb
@@ -0,0 +1,8 @@
+
+# sales.devudo.com is simply another aegir instance, but with super-powers.
+# This server will fire up other servers with deployed devmaster instances.
+
+# First get Aegir (which also gets lamp and users)
+
+# @TODO: Figure out how to change attributes here
+require_recipe "devudo::aegir"
