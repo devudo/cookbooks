94f67f77647811ae1d3389bbf319cd1a52b3e9f4
Jon Pugh <jon@thinkdrop.net>
2013-02-23 12:55:21 -0500

    fixing version

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "jon@thinkdrop.net"
 license          "N/A PROPRIETARY & CONFIDENTIAL"
 description      "Installs/Configures servers to run devudo.com"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.x"
+version          "1.0"
