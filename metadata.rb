8a2dc8851fe0bacab1835da343e7049a8c8bbe2c
Jon Pugh <jon@thinkdrop.net>
2013-02-24 16:24:55 -0500

    requiring apt cookbook so that updates take place properly.

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,5 @@ license          "N/A PROPRIETARY & CONFIDENTIAL"
 description      "Installs/Configures servers to run devudo.com"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0"
+
+depends "apt"
