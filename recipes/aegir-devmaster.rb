4c7f3dd0a05f271c6af37de51bab76512de700bb
Jon Pugh <jon@thinkdrop.net>
2013-02-24 21:48:07 -0500

    adding shopmaster's public key to the devmaster profile for automatic access!

--- recipes/aegir-devmaster.rb
+++ recipes/aegir-devmaster.rb
@@ -7,6 +7,35 @@
 # we could tell the aegir recipe to install devmaster instead of hostmaster!
 require_recipe "devudo::aegir"
 
+# give access to shopmaster
+cookbook_file "/var/aegir/.ssh/authorized_keys" do
+  source "ssh/id_rsa.pub.shopmaster"
+  action :create
+  backup false
+  owner "aegir"
+  group "aegir"
+  mode "0600"
+end
+
+# give access to all our users.
+userlist = node[:devudo][:users]
+
+group "devs" do
+  action :create
+  group_name "devs"
+end
+
+## Users
+#node[:devudo][:users].each{|username|
+#  cookbook_file "/var/aegir/.ssh/id_rsa.pub.#{username}" do
+#    source "ssh/id_rsa.pub.#{username}"
+#    action :create
+#    backup false
+#    owner "#{username}"
+#    group "#{username}"
+#    mode "0600"
+#  end
+#}
 
 # prepare drush commands folder
 directory "/var/aegir/hostmaster-6.x-1.x/sites/all/modules/" do
