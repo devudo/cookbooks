33d83849a1f00d81956e756da31ca46b203ea900
Jon Pugh <jon@thinkdrop.net>
2013-02-26 11:59:35 -0500

    better shopmaster, removing wrong modules, syncing repos instead.

--- recipes/aegir-shopmaster.rb
+++ recipes/aegir-shopmaster.rb
@@ -1,3 +1,6 @@
+#####################
+# DEVUDO SHOPMASTER #
+#####################
 
 # sales.devudo.com is simply another aegir instance, but with super-powers.
 # This server will fire up other servers with deployed devmaster instances.
@@ -18,29 +21,27 @@ directory "/var/aegir/hostmaster-6.x-1.x/sites/all/modules/" do
 end
 
 # Hostmaster enhancements
-git "/var/aegir/hostmaster-6.x-1.x/sites/all/modules/devudo_hosting" do
-    repository "git@github.com:devudo/devudo_hosting.git"
-    reference "6.x-1.x"
-    action :export
+git "/var/aegir/hostmaster-6.x-1.x/sites/all/modules/shop_hosting" do
+    repository "git@github.com:devudo/shop_hosting.git"
+    action :sync
     user "aegir"
 end
 git "/var/aegir/hostmaster-6.x-1.x/sites/all/modules/hosting_logs" do
     repository "http://git.drupal.org/project/hosting_logs.git"
     reference "6.x-1.x"
-    action :export
+    action :sync
     user "aegir"
 end
 
 # Provision tools
-git "/var/aegir/.drush/devudo_provision" do
-    repository "git@github.com:devudo/devudo_provision.git"
-    reference "6.x-1.x"
-    action :export
+git "/var/aegir/.drush/shop_provision" do
+    repository "git@github.com:devudo/shop_provision.git"
+    action :sync
     user "aegir"
 end
 git "/var/aegir/.drush/provision_logs" do
     repository "http://git.drupal.org/project/provision_logs.git"
     reference "6.x-1.x"
-    action :export
+    action :sync
     user "aegir"
 end
