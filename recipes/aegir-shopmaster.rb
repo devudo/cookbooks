07e11d337466a9fec2afd337817a251c29d079f9
Jon Pugh <jon@thinkdrop.net>
2013-02-27 15:16:09 -0500

    adding ssh key module

--- recipes/aegir-shopmaster.rb
+++ recipes/aegir-shopmaster.rb
@@ -26,9 +26,9 @@ git "/var/aegir/hostmaster-6.x-1.x/sites/all/modules/shop_hosting" do
     action :sync
     user "aegir"
 end
-git "/var/aegir/hostmaster-6.x-1.x/sites/all/modules/hosting_logs" do
-    repository "http://git.drupal.org/project/hosting_logs.git"
-    reference "6.x-1.x"
+git "/var/aegir/hostmaster-6.x-1.x/sites/all/modules/sshkey" do
+    repository "http://git.drupal.org/project/sshkey.git"
+    reference "6.x-2.0"
     action :sync
     user "aegir"
 end
@@ -36,12 +36,6 @@ end
 # Provision tools
 git "/var/aegir/.drush/shop_provision" do
     repository "git@github.com:devudo/shop_provision.git"
-    action :sync
-    user "aegir"
-end
-git "/var/aegir/.drush/provision_logs" do
-    repository "http://git.drupal.org/project/provision_logs.git"
-    reference "6.x-1.x"
     action :sync
     user "aegir"
 end
