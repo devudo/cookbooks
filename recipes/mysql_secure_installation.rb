0cd51fabbed7280adaea73820b5b83e5d210b55f
Jon Pugh <jon@thinkdrop.net>
2013-02-27 15:16:23 -0500

    better handling of mysql secure installation.

--- recipes/mysql_secure_installation.rb
+++ recipes/mysql_secure_installation.rb
@@ -1,9 +1,10 @@
 execute "Secure MySQL Installation" do
-  command "\"#{node['mysql']['mysql_bin']}\" -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < \"/tmp/secure_installation.sql\""
+  command "\"#{node['mysql']['mysql_bin']}\" -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < \"/etc/mysql/secure_installation.sql\""
   action :nothing
 end
 
-cookbook_file "/tmp/secure_installation.sql" do
+cookbook_file "/etc/mysql/secure_installation.sql" do
   source "secure_installation.sql"
   notifies :run, resources(:execute => "Secure MySQL Installation"), :immediately
+  not_if {File.exists?("/etc/mysql/secure_installation.sql")}
 end
