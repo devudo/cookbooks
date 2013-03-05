a53b8add1997b4ec3073ae74ecd948ca0ed31a35
Jon Pugh <jon@thinkdrop.net>
2013-03-05 14:18:54 -0500

    removing repos that are already in our repo.

--- recipes/aegir-devmaster.rb
+++ recipes/aegir-devmaster.rb
@@ -16,51 +16,6 @@ git aegir_root do
   group "aegir"
 end
 
-# give access to all our users.
-userlist = node[:devudo][:users]
-
-group "devs" do
-  action :create
-  group_name "devs"
-end
-
-
-# prepare aegir modules folder
-directory "#{aegir_root}/sites/all/modules/" do
-  owner "aegir"
-  group "aegir"
-  mode 00755
-  action :create
-  recursive true
-end
-
-# Hostmaster enhancements
-# @TODO: All unneccessary when we get a devmaster make file.
-#git "#{aegir_root}/sites/all/modules/devshop_hosting" do
-#    repository "http://git.drupal.org/project/devshop_hosting.git"
-#    reference "6.x-1.x"
-#    action :sync
-#    user "aegir"
-#end
-#git "/var/aegir/hostmaster-6.x-1.x/sites/all/modules/ctools" do
-#    repository "http://git.drupal.org/project/ctools.git"
-#    reference "6.x-1.10"
-#    action :sync
-#    user "aegir"
-#end
-#git "/var/aegir/hostmaster-6.x-1.x/sites/all/modules/hosting_solr" do
-#    repository "http://git.drupal.org/project/hosting_solr.git"
-#    reference "6.x-1.x"
-#    action :sync
-#    user "aegir"
-##end
-#git "/var/aegir/hostmaster-6.x-1.x/sites/all/modules/hosting_logs" do
-#    repository "http://git.drupal.org/project/hosting_logs.git"
-#    reference "6.x-1.x"
-#    action :sync
-#    user "aegir"
-#end
-
 # Provision tools
 git "/var/aegir/.drush/provision_git" do
     repository "http://git.drupal.org/project/provision_git.git"
