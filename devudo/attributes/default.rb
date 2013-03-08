2fef6df8beb3cddaa1e5b0cdc6179a3b6f2bbce7
Jon Pugh <jon@thinkdrop.net>
2013-03-07 22:06:00 -0500

    removing chef-solo attributes

--- devudo/attributes/default.rb
+++ devudo/attributes/default.rb
@@ -9,12 +9,6 @@ default[:aegir][:db_host] = "localhost"
 default[:aegir][:profile] = "hostmaster"
 default[:aegir][:makefile] = "/var/aegir/.drush/provision/aegir.make"
 
-# Needs to be set for chef SOLO only.  For chef server, it will be random
-# @TODO! Remove this from cookbook, put it in vagrantfile
-node[:mysql][:server_root_password] = "abcd12345678"
-node[:mysql][:server_debian_password] = "abcd12345678"
-node[:mysql][:server_repl_password] = "abcd12345678"
-
 # System users to create on the target
 # Put the users authorized_keys as the value of this array
 default[:devudo][:users] = {
