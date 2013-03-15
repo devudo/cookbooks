400ff4c4a30d695b28ff043a1d36e3d2a79d5748
Jon Pugh <jon@thinkdrop.net>
2013-03-14 22:28:41 -0400

    attempting to use runit instead

--- devudo/attributes/aegir-devmaster.rb
+++
@@ -1,3 +0,0 @@
-default[:aegir][:profile] = "devmaster"
-default[:aegir][:makefile] = "#{node[:aegir][:dir]}/.drush/devudo_provision/build-devmaster.make"
-default[:aegir][:hostmaster_install_command] = "devmaster-install"
