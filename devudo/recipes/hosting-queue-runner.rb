88ff64b27321a41ec323fc3cdcecfdfa635d1aaa
Jon Pugh <jon@thinkdrop.net>
2013-03-25 18:36:02 -0400

    fixing up hosting-queue-runner so it doesn't run if its already there

--- devudo/recipes/hosting-queue-runner.rb
+++ devudo/recipes/hosting-queue-runner.rb
@@ -1,15 +1,13 @@
 
 
-# Get provision.
-drush "@hostmaster dl hosting_queue_runner" do
-  not_if do
-    File.exists?("#{node[:aegir][:dir]}/#{node[:aegir][:profile]}-#{node[:aegir][:version]}/sites/all/modules/hosting_queue_runner")
-  end
+# Get hosting_queue_runner (if its not there yet.)
+if File.exists?("#{node[:aegir][:dir]}/#{node[:aegir][:profile]}-#{node[:aegir][:version]}/sites/all/modules/hosting_queue_runner")
+    
+  drush "@hostmaster dl hosting_queue_runner" 
+  
+  # @TODO: Enable hosting_queue-runner in @hostmaster
+  include_recipe "runit"
+  runit_service "hosting-queue"
+  
+  drush "@hostmaster en hosting_queue_runner" 
 end
-
-# @TODO: Enable hosting_queue-runner in @hostmaster
-include_recipe "runit"
-runit_service "hosting-queue"
-
-# Get provision.
-drush "@hostmaster en hosting_queue_runner"
