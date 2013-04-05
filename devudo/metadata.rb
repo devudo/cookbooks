7fdbc49714712946336533dcd1d84cfcd9f08a3d
Jon Pugh <jon@thinkdrop.net>
2013-04-05 01:18:11 -0400

    removing solr.war and updating version

--- devudo/metadata.rb
+++ devudo/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "jon@thinkdrop.net"
 license          "N/A PROPRIETARY & CONFIDENTIAL"
 description      "Installs/Configures servers to run devudo.com"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0"
+version          "1.1"
 
 %w{ drush apt sudo nginx mysql openssl php php-fpm runit}.each do |cb|
   depends cb
