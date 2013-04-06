872e1222be763ef12d01524cb8b5dcb59c6c94aa
Jon Pugh <jon@thinkdrop.net>
2013-04-06 16:51:28 -0400

    reordering so the php cookbook apache templat works

--- devudo/recipes/lamp.rb
+++ devudo/recipes/lamp.rb
@@ -4,11 +4,10 @@
 include_recipe "apt"
 include_recipe "mysql::server"
 include_recipe "devudo::mysql_secure_installation"
-include_recipe "drush"
 
 # Install required apt packages.
 # @TODO: Make these a default attribute
-%w{ apache2 libapache2-mod-php5 postfix sudo rsync git-core unzip vim }.each do |package_name|
+%w{ apache2 postfix sudo rsync git-core unzip vim }.each do |package_name|
   package package_name
 end
 
@@ -27,10 +26,12 @@ end
 
 # PHP must be included after apache is available, because
 # the /etc/php5/apache2 folder doesn't exist yet.
+package "libapache2-mod-php5"
 include_recipe "php"
+include_recipe "drush"
 
 # additional php packages
-%w{ php5 php5-cli php5-gd php5-mysql php-pear php5-curl  }.each do |package_name|
+%w{ php5 php5-cli php5-gd php5-mysql php-pear php5-curl }.each do |package_name|
   package package_name
 end
 
