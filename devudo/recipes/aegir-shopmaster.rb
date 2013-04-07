d60e5530d00a19d02b8a7f15f1602dea72f8c9a8
Jon Pugh <jon@thinkdrop.net>
2013-04-06 21:14:17 -0400

    cleaning up comments

--- devudo/recipes/aegir-shopmaster.rb
+++ devudo/recipes/aegir-shopmaster.rb
@@ -2,13 +2,12 @@
 # DEVUDO SHOPMASTER #
 #####################
 
-# sales.devudo.com is simply another aegir instance, but with super-powers.
+# shop.devudo.com is simply another aegir instance, but with super-powers.
 # This server will fire up other servers with deployed devmaster instances.
 
 node.set[:aegir][:profile] = "shopmaster"
 node.set[:aegir][:makefile] = "#{node[:aegir][:dir]}/.drush/devudo_provision/build-shopmaster.make"
 node.set[:aegir][:hostmaster_install_command] = "devmaster-install"
-
 
 aegir_root = node[:aegir][:dir]
 shopmaster_root = "#{node[:aegir][:dir]}/#{node[:aegir][:profile]}-#{node[:aegir][:version]}"
@@ -16,33 +15,7 @@ shopmaster_root = "#{node[:aegir][:dir]}/#{node[:aegir][:profile]}-#{node[:aegir
 # First get Aegir (which also gets lamp and users)
 include_recipe "devudo::aegir"
 
-# Link up shop_provision
+# Link up shop_provision.  We store this in the shopmaster repo now.
 link "#{node[:aegir][:dir]}/.drush/shop_provision" do
   to "#{shopmaster_root}/profiles/shopmaster/drush/shop_provision"
 end
-
-
-#
-## don't forget our shop_provision
-#git "#{node[:aegir][:dir]}/.drush/shop_provision" do
-#  repository "git@github.com:devudo/shop_provision.git"
-#  reference "master"
-#  action :sync
-#  enable_submodules true
-#  user "aegir"
-#  group "aegir"
-#end
-
-
-# @TODO: How to update all working-copy git repos
-# within shopmaster!
-# This repo was originally created with the makefile by devmaster-install
-# Make sure the repo is up to date
-#git shopmaster_root do
-#  repository "git@github.com:devudo/shopmaster.git"
-#  reference "6.x-1.x"
-#  action :sync
-#  enable_submodules true
-#  user "aegir"
-#  group "aegir"
-#end
