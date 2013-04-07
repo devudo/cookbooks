75772d2bd0745cda9d36cbd4113e584eeee6a837
Jon Pugh <jon@thinkdrop.net>
2013-04-07 12:50:24 -0400

    adding max allowed packets

--- devudo/attributes/default.rb
+++ devudo/attributes/default.rb
@@ -22,6 +22,7 @@ default['drush']['version'] = "7.x-4.6"
 
 default['php']['conf_dir']      = '/etc/php5/apache2'
 
+default['mysql']['tunable']['max_allowed_packet']   = "64M"
 
 # System users to create on the target
 # Put the users authorized_keys as the value of this array
