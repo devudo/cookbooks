2b455e6137ac77c8ee85a33b5de92893208636e8
Jon Pugh <jon@thinkdrop.net>
2013-04-10 17:45:32 -0400

    adding knife-rackspace recipe

---
+++ devudo/recipes/knife-rackspace.rb
@@ -0,0 +1,33 @@
+
+include_recipe "apt"
+
+
+# knife rackspace server create
+# Its a gem, but dang it has a lot of needs.
+
+
+# APR 10 attempt
+# FROM http://nokogiri.org/tutorials/installing_nokogiri.html
+#sudo apt-get install ruby1.8-dev ruby1.8 ri1.8 rdoc1.8 irb1.8 libreadline-ruby1.8 libruby1.8 libopenssl-ruby libxslt-dev libxml2-dev
+#
+#sudo apt-get install make
+#sudo gem install nokogiri
+#sudo gem install fog --version 1.8.0
+#sudo gem install knife-rackspace
+# IT WORKED!!!!!!
+
+
+%w{ make ruby1.8-dev ruby1.8 ri1.8 rdoc1.8 irb1.8 libreadline-ruby1.8 libruby1.8 libopenssl-ruby libxslt-dev libxml2-dev }.each do |package_name|
+  package package_name
+end
+
+gem_package "nokogiri" do
+  options("--no-rdoc --no-ri")
+end
+gem_package "fog" do
+  options("--version 1.8.0 --no-rdoc --no-ri")
+end
+gem_package "knife-rackspace" do
+  options("--no-rdoc --no-ri")
+end
+
