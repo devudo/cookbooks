3d4b8ea16792aa37f853dc4313d4140e58c971f6
Jon Pugh <jon@thinkdrop.net>
2013-04-12 18:31:58 -0400

    adding newrelic

--- devudo/metadata.rb
+++ devudo/metadata.rb
@@ -5,6 +5,6 @@ description      "Installs/Configures servers to run devudo.com"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.1"
 
-%w{ drush apt sudo nginx mysql openssl php php-fpm runit}.each do |cb|
+%w{ drush apt sudo newrelic nginx mysql openssl php php-fpm runit}.each do |cb|
   depends cb
 end
