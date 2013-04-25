e398a9900c533c621805e42ab673524baae225b8
Jon Pugh <jon@thinkdrop.net>
2013-04-25 16:54:22 -0400

    adding source code linking to vagrant recipe.

---
+++ devudo/recipes/vagrant.rb
@@ -0,0 +1,27 @@
+#
+# This file will remove existing git repos and replace with links
+# to /source repos.
+#
+
+sourceList = {
+  'devudo/devudo_provision' => '/var/aegir/.drush/devudo_provision',
+  'drupal/provision_git' => '/var/aegir/.drush/provision_git',
+  'drupal/provision_solr' => '/var/aegir/.drush/provision_solr',
+  'drupal/provision_logs' => '/var/aegir/.drush/provision_logs',
+  'drupal/devshop_provision' => '/var/aegir/.drush/devshop_provision'
+}
+
+sourceList.each{|source_path, original_path|
+  if File.directory?(original_path)
+    directory original_path do
+      action :delete
+      recursive true
+    end
+    link original_path do
+      to "/source/#{source_path}"
+      group "aegir"
+      owner "aegir"
+    end  
+  end
+}
+
