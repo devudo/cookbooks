d92a04cedfc7ad5cbdd13a22300ef418f5bcc2db
Jon Pugh <jon@thinkdrop.net>
2013-05-03 13:21:00 -0400

    including github-deploy in devudo metadata

--- devudo/metadata.rb
+++ devudo/metadata.rb
@@ -5,6 +5,6 @@ description      "Installs/Configures servers to run devudo.com"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.1"
 
-%w{ drush apt sudo newrelic nginx mysql openssl php php-fpm runit ssh_known_hosts}.each do |cb|
+%w{ drush apt sudo newrelic nginx mysql openssl php php-fpm runit ssh_known_hosts github-deploys}.each do |cb|
   depends cb
 end
