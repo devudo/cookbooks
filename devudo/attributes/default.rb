427389d690c400c997daf1d57417e5b1d8dbfb3d
Jon Pugh <jon@thinkdrop.net>
2013-05-06 12:57:33 -0400

    merge conflict

--- devudo/attributes/default.rb
+++ devudo/attributes/default.rb
@@ -57,15 +57,3 @@ default[:devudo][:users] = {
   'devudo' => '',
 }
 
-# Generate and deploy aegir ssh key to github
-node.set['github_deploys']['deploy_user'] = "github"
-node.set['github_deploys']['deploy_group'] = "github"
-node.set['github_deploys']['deploy_home_path'] = "/var/github"
-
-node.set['github_deploys']['github_api']['endpoint_path'] = "/user/keys"
-node.set['github_deploys']['github_api']['username'] = "devudo-deploy"
-node.set['github_deploys']['github_api']['email'] = "jon@devudo.com"
-node.set['github_deploys']['github_api']['password'] = "wakkawakka123"
-node.set['github_deploys']['github_api']['user_agent'] = "Devudo/1.0"
-
-
